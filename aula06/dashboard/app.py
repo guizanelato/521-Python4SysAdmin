c1202391f94559acb0e0312c4ce078cdac6cd4b5
Guilherme Zanelato Corrêa <guilherme.zanelato@gmail.com>
2019-10-08 20:36:08 -0300

    commit no gitlab local

---
+++ aula06/dashboard/app.py
@@ -0,0 +1,25 @@
+
+import os
+import sys
+
+import flask
+
+from routes.docker import blueprint as docker
+from routes.jenkins import blueprint as jenkins
+from routes.auth import blueprint as auth
+from routes.gitlab import blueprint as gitlab
+
+
+app = flask.Flask(__name__)
+
+app.secret_key =  'secret'
+
+app.register_blueprint(docker)
+app.register_blueprint(jenkins)
+app.register_blueprint(auth)
+app.register_blueprint(gitlab)
+
+#aqui só pode ser configuração, nada de lógica
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
